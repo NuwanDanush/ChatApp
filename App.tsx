f0323c878c617952a13e0a92dd060b965f863ee4
NuwanDanush <danushkanuwan252@gmail.com>
2020-07-10 21:00:00 +0530

    colours

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Container, Header } from "native-base";
 export default function App() {
   return (
     <View style={styles.container}>
