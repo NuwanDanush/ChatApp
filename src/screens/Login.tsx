3e5ff8d44aa10b3c5ce14f9bd7f241174e95d677
NuwanDanush <danushkanuwan252@gmail.com>
2020-07-11 03:14:49 +0530

    button

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,35 +1,47 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, } from 'react-native';
-import {Button} from 'native-base';
-import {RouteProp} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+import { Button, Form } from 'native-base';
+import { RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import { AuthParamList } from '../types';
+import Colours from '../constants/Colours';
 
 type LoginRouteProps = RouteProp<AuthParamList, 'Login'>;
 type LoginNavigationProps = StackNavigationProp<AuthParamList, 'Login'>;
 
-type props =  {
-  route :LoginRouteProps,
+type props = {
+  route: LoginRouteProps,
   navigation: LoginNavigationProps
 }
 
-const Login = ({route, navigation}:props) => {
-  return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-      <Button onPress={()=>navigation.push('Register')}>
-        <Text>Register</Text>
-      </Button>
-    </View>
-  );
+const Login = ({ route, navigation }: props) => {
+
+    const [fname, onChangeText] = React.useState('Nuwan');
+    const [lname, onChangelname] = React.useState('danushka');
+  
+    return (
+      <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:Colours.GRAY}}>
+        <Text>{fname}</Text>
+        <Text>{lname}</Text>
+        <Button onPress={()=>{onChangeText('pakaya'),onChangelname('huthths')}}> 
+          <Text>Button</Text>
+        </Button>
+      </View>
+      // <TextInput
+      //   style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      //   onChangeText={text => onChangeText(text)}
+      //   value={value}
+      // />
+    );
 };
 
 export default Login;
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
+
