4b9cac2ab86770d44c8f66c7f2fc090a4140fc86
NuwanDanush <danushkanuwan252@gmail.com>
2020-07-27 22:41:58 +0530

    addchat

--- src/screens/Chat/AddChat.tsx
+++ src/screens/Chat/AddChat.tsx
@@ -13,13 +13,93 @@ const AddChat = (props: AddChatProps) => {
     const ref = firebase.database();
     const [userList, setUserList] = React.useState<Array<User>>([])
     const [isLoading, setLoading] = React.useState<boolean>(true)
-}
+
 React.useEffect(() => {
     loadData()
 }, [])
 
 const loadData = () =>{
-    
+    setLoading(true);
+        ref.ref('/User/').once('value')
+            .then((snapshot) => {
+                setUserList([]);
+                const userData = snapshot.val()
+                let friends: Array<string | undefined> = [];
+                let myData: User = {};
+                if (user?.uid) {
+                    myData = userData[user?.uid]
+                    friends = myData.friends ? myData.friends : []
+                }
+                else {
+                    setLoading(false);
+                    alert('Error occured!')
+                    return;
+                }
+                if (friends.length !== 0) {
+                    friends.map((value, i) => {
+                        delete userData[value ? value : ""]
+                    })
+                }
+                for (let key in userData) {
+                    if (key !== user.uid) {
+                        let tempUser: User = userData[key];
+                        tempUser.uid = key;
+                        setUserList(prevState => [...prevState, tempUser])
+                        setLoading(false);
+                    }
+                }
+            })
+            .catch((error) => {
+                setLoading(false);
+                alert(error.message)
+            })
+}
+const add = (uid: string | undefined) => {
+    setLoading(true)
+    console.log(uid);
+    ref.ref('/User/' + user?.uid + '/').once('value')
+        .then((snapshot) => {
+            const mydata: User = snapshot.val();
+            let tempFriends = mydata.friends;
+            let chats = mydata.chats;
+            tempFriends = tempFriends ? [...tempFriends, uid] : [uid];
+            const newChat: Chat = {
+                users: [user?.uid, uid],
+            }
+            const chatId = ref.ref('/Chat/').push(newChat)
+
+            chats = chats ? [...chats, chatId.key] : [chatId.key];
+
+            ref.ref('/User/' + uid + '/').once('value')
+                .then((snapshot) => {
+                    const data: User = snapshot.val();
+                    const temp = data.friends ? [...data.friends, user?.uid] : [user?.uid];
+                    const chat = data.chats ? [...data.chats, chatId.key] : [chatId.key];
+                    data.friends = temp;
+                    data.chats = chat;
+                    ref.ref('/User/' + uid + '/').update(data)
+                        .then(() => {
+                            ref.ref('/User/' + user?.uid + '/').update({ friends: tempFriends, chats: chats })
+                                .then(() => {
+                                    loadData();
+                                    setLoading(false)
+                                })
+                                .catch(error => {
+                                    setLoading(false)
+                                    alert(error.message)
+                                })
+                        })
+                        .catch((error) => {
+                            setLoading(false);
+                            alert(error.message)
+                        })
+                })
+        })
+        .catch(error => {
+            setLoading(false)
+            alert(error.message)
+        })
+}
 }
 export default AddChat;
 
