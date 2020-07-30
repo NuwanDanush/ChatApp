39e84f87e630e0cb7a2d8e08ab76dd684b7af213
NuwanDanush <danushkanuwan252@gmail.com>
2020-07-30 18:40:42 +0530

    ChatView

--- src/screens/Chat/ChatView.tsx
+++ src/screens/Chat/ChatView.tsx
@@ -39,3 +39,21 @@ type UserWithChatId = {
   user:User;
   chatId:string|null|undefined;
 }
+interface ChatListProps {}
+const ChatList = (props: Props) => {
+  const user = firebase.auth().currentUser;
+  const ref = firebase.database();
+  const [chats, setChats] = React.useState<Array<Chat>>([]);
+  const [userArray, setUser] = React.useState<Array<UserWithChatId>>([])
+  const [isLoading, setLoading] = React.useState<boolean>(true);
+
+  React.useEffect(()=>{
+    loadChats();
+    return()=>{
+      setChats([])
+    }
+  },[])
+
+  const loadChats = () => {
+    
+  }
